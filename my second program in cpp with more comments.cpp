fa9d52ba12b9e133b1831462ad5fce620876a33e
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-15 09:25:39 -0400

    Add files via upload

---
+++ my second program in cpp with more comments.cpp
@@ -0,0 +1,10 @@
+/* my second program in C++
+with more comments */
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World!" ; //prints Hello World!
+    std::cout<<"I am a C++ programmer" ; //print I am a C++ programmer
+}

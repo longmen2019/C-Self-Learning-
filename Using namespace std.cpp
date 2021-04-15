cea3293c79aef171e10100415099519a72f34183
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-15 09:33:40 -0400

    Add files via upload

---
+++ Using namespace std.cpp
@@ -0,0 +1,13 @@
+/* Using namespace std declaration to allows all elements in the std
+namespace in an unqualified manner( without the std::prefix)*/
+
+#include <iostream>
+using namespace std
+
+int main()
+{
+    cout << "Hello World!";
+    cout << "I am a C++ program";
+}
+
+                                                                                                      without the std::prefix)*/

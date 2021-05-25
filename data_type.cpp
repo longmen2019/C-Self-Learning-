b3d824431eb9efe17c7c729a9b4732c8619d543f
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-05-25 09:46:18 -0400

    Add files via upload

---
+++ data_type.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char grade = 'A';
+  string phrase = "Long Men";
+  int age = 30;
+  double gpa = 2.3;
+  bool isMale = false;
+
+  cout << gpa <<endl;
+  return 0 ; 
+
+
+}

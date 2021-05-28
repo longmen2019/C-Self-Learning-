dc7e0f1fc33b06815ae6270c83be8f5084e23368
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-05-28 15:49:04 -0400

    Add files via upload

---
+++ building_a_calculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int num1 ,num2;
+  //This will take input for the first number
+  cout << "Enter first number:";
+  //This will store the first input
+  cin >> num1;
+
+  //This wil take input for the second number
+  cout << "Enter second number:";
+
+  //This will store the second input
+  cin >> num2;
+
+  //This will do the addition between num1 and num2
+  cout << num1 + num2; 
+
+  return 0; 
+}
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+  // substitute integer with double
+  double num1 ,num2;
+  cout << "Enter first number: ";
+  cin >> num1;
+
+  cout << "Enter second number:";
+  cin >> num2;
+
+  cout << num1 + num2;
+  return 0; 
+}
